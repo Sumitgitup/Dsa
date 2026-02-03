08d4a235594e2d1a9af1a02e6b2799e3ff1afdee
Sumitgitup <sumitroy7august@gmail.com>
2026-02-03 20:11:19 +0530

    added solution

--- solutions/pract.js
+++ solutions/pract.js
@@ -161,4 +161,20 @@
 
 
 
+const nums = [1,1,1,1,2];
+// Output: k = 2, nums = [1,2,_]
 
+var removeDuplicates = function(nums) {
+    if (nums.length < 2) return nums.length;
+
+    let i = 0;
+    for (let j = 1; j < nums.length; j++) { 
+        if (nums[j] !== nums[i]) {
+            i++; 
+            nums[i] = nums[j]; 
+        }
+    }
+    return i+1;
+};
+
+console.log(removeDuplicates(nums));
--- solutions/session8.js
+++ solutions/session8.js
@@ -1,21 +1,21 @@
 // Question: https://leetcode.com/problems/remove-duplicates-from-sorted-array-ii/submissions/1890809605/
 
 const nums = [1, 1, 1, 2, 2, 3];
-//Output: 5, nums = [0,0,1,1,2,3,3,_,_]
+// Output: 5, nums = [0,0,1,1,2,3,3,_,_]
 
-// var removeDuplicates = function(nums) {
-//     if (nums.length <= 2) return nums;
+var removeDuplicates = function(nums) {
+    if (nums.length <= 2) return nums;
 
-//     let write = 2;
-//     for (let i = 2; i < nums.length; i++) {
-//         if (nums[i] !== nums[write-2]) {
-//             nums[write] = nums[i];
-//             write++;
-//         }
-//     }
-//     return write
-// //    return nums
-// };
+    let write = 2;
+    for (let i = 2; i < nums.length; i++) {
+        if (nums[i] !== nums[write-2]) {
+            nums[write] = nums[i];
+            write++;
+        }
+    }
+    // return write
+   return nums
+};
 
 // console.log(removeDuplicates(nums));
 
